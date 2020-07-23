ebea5a544f50a1824d06768a67e54f359c0bf686
jhj9109 <jhj91_09@naver.com>
2020-07-23 18:10:02 +0900

    Update RestaurantReview.vue | 더미데이터 멘트 수정

--- frontend/src/views/review/RestaurantReview.vue
+++ frontend/src/views/review/RestaurantReview.vue
@@ -45,7 +45,7 @@
       reviews: [
         {
           imgUrl: "https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665__340.jpg",
-          rank: 4.5,
+          rank: 5.0,
           content: "이곳 가게 이름답게 어썸한 가게 인정? 인정"
         },
         {
@@ -55,13 +55,13 @@
         },
         {
           imgUrl: "https://cdn.pixabay.com/photo/2016/10/22/20/34/wine-1761613__340.jpg",
-          rank: 2.0,
-          content: "이곳 가게 이름답게 어썸한 가게 인정? 인정"
+          rank: 4.0,
+          content: "전 분위기도 좋고 괜찮았어요! 저녁의 여유로움~"
         },
         {
           imgUrl: "https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg",
-          rank: 1.0,
-          content: "위에 리뷰에 속았다. 분하다 여러분 속지 마세요. 이 가게 매우 어글리하고 맛없는 가게임!"
+          rank: 3.5,
+          content: "강력!!!! 추천은 아니지만.... ㅎㅎ 괜찮은 식당이에요 ^^"
         },
       ]
     }),
