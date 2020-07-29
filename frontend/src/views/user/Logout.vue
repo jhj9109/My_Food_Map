b74126c1e4606aa7780f617e4ec8631aa9d5df27
jhj9109 <jhj91_09@naver.com>
2020-07-30 03:44:59 +0900

    Add vue | 로그아웃 페이지 구현, 로그인페이지로 리다이렉트, replace 활용 |

---
+++ frontend/src/views/user/Logout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="user" id="logout">
+  </div>
+</template>
+
+<script>
+import {  mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions ({
+      logout: 'user/logout'
+    })
+  },
+  // 아래와 동일한 기능 & 다른 타이밍
+  // beforeRouteEnter(to, from, next) {
+  //   console.log("로그아웃.vue 진입1 =>")
+  //   console.log("this : ", this)
+  //   const token = localStorage.token
+  //   next(vm => {
+  //       console.log("vm 진입", vm)
+  //       vm.$store.dispatch('user/logout', {token : token})
+  //       console.log("vm.$router.push")
+  //       vm.$router.push({ name: 'Login'})
+  //   })
+  // },
+  beforeMount() {
+    const token = localStorage.token
+    const data = {
+      token: token
+    }
+    console.log("로그아웃.vue 진입2 => token값은 : ", token, data.token)
+    this.$store.dispatch('user/logout', data)
+  }
+
+};
+</script>
+
+
