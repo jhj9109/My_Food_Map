45619e290e5008e57a7daf26321e57b8c2d18db9
kmathl96 <kmathl96@naver.com>
2020-07-30 22:47:52 +0900

    Edit review.sql

--- backend/sql/ssafysk_review.sql
+++ backend/sql/ssafysk_review.sql
@@ -24,7 +24,8 @@ DROP TABLE IF EXISTS `review`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `review` (
   `no` int NOT NULL AUTO_INCREMENT,
-  `userid` int DEFAULT NULL,
+  `userid` int NOT NULL,
+  `resid` int NOT NULL,
   `content` varchar(255) DEFAULT NULL,
   `rank` int NOT NULL,
   `create_date` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -32,7 +33,9 @@ CREATE TABLE `review` (
   `like_cnt` int DEFAULT 0,
   PRIMARY KEY (`no`),
   KEY `userid` (`userid`),
-  CONSTRAINT `review_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
+  KEY `resid` (`resid`),
+  CONSTRAINT `review_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`),
+  CONSTRAINT `review_ibfk_2` FOREIGN KEY (`resid`) REFERENCES `restaurant` (`resid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -42,7 +45,7 @@ CREATE TABLE `review` (
 
 LOCK TABLES `review` WRITE;
 /*!40000 ALTER TABLE `review` DISABLE KEYS */;
-INSERT INTO `review` VALUES (1,1,'맛있는데 비싸서 1점 깎았습니다.',4,'2020-07-30 00:00:00',NULL,0),(2,2,'웨이팅 1시간 해서 짜증났는데 음식 먹는 순간 다 풀림',5,'2020-07-30 00:00:00',NULL,0),(3,3,'jmt,, 사장님이 쫌 불친절하시지만 너무 맛있어서 다시 갈 거예요,,,',4,'2020-07-15 00:00:00',NULL,0),(4,5,'음식에서 머리카락 나오고 진짜 최악;;; 절대 가지 마세요ㅜㅜㅜ',1,'2020-07-21 00:00:00',NULL,0),(5,7,'그냥저냥 평범ㅇㅇ 재방문 하진 않을 듯 ?',3,'2020-07-08 00:00:00',NULL,0);
+INSERT INTO `review` VALUES (1,1,1,'맛있는데 비싸서 1점 깎았습니다.',4,'2020-07-30 00:00:00',NULL,0),(2,2,2,'웨이팅 1시간 해서 짜증났는데 음식 먹는 순간 다 풀림',5,'2020-07-30 00:00:00',NULL,0),(3,3,3,'jmt,, 사장님이 쫌 불친절하시지만 너무 맛있어서 다시 갈 거예요,,,',4,'2020-07-15 00:00:00',NULL,0),(4,5,4,'음식에서 머리카락 나오고 진짜 최악;;; 절대 가지 마세요ㅜㅜㅜ',1,'2020-07-21 00:00:00',NULL,0),(5,7,5,'그냥저냥 평범ㅇㅇ 재방문 하진 않을 듯 ?',3,'2020-07-08 00:00:00',NULL,0);
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
