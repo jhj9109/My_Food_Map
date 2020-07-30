13359c185cc6aa93d448637b9e28269cdad0d5ce
kmathl96 <kmathl96@naver.com>
2020-07-30 14:46:25 +0900

    Create SQL Files

---
+++ backend/sql/createReview.sql
@@ -0,0 +1,5 @@
+INSERT INTO `ssafysk`.`review` (`no`, `userid`, `content`, `rank`, `create_date`) VALUES ('1', '1', '맛있는데 비싸서 1점 깎았습니다.', '4', '2020-07-30');
+INSERT INTO `ssafysk`.`review` (`no`, `userid`, `content`, `rank`, `create_date`) VALUES ('2', '2', '웨이팅 1시간 해서 짜증났는데 음식 먹는 순간 다 풀림', '5', '2020-07-30');
+INSERT INTO `ssafysk`.`review` (`no`, `userid`, `content`, `rank`, `create_date`) VALUES ('3', '3', 'jmt,, 사장님이 쫌 불친절하시지만 너무 맛있어서 다시 갈 거예요,,,', '4', '2020-07-15');
+INSERT INTO `ssafysk`.`review` (`no`, `userid`, `content`, `rank`, `create_date`) VALUES ('4', '5', '음식에서 머리카락 나오고 진짜 최악;;; 절대 가지 마세요ㅜㅜㅜ', '1', '2020-07-21');
+INSERT INTO `ssafysk`.`review` (`no`, `userid`, `content`, `rank`, `create_date`) VALUES ('5', '7', '그냥저냥 평범ㅇㅇ 재방문 하진 않을 듯 ?', '3', '2020-07-08');
