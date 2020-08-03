852c17e347208463eca14ff4b788f150765768d7
jhj9109 <jhj91_09@naver.com>
2020-08-03 14:10:35 +0900

    Add RestaurantApi.js |음식점 관련 API.js 생성|
    좋아요, 리스트요청, 1개요청, 생성

---
+++ frontend/src/api/RestaurantApi.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+const hosturl = 'http://localhost:9999'
+const appname = '/restaurant'
+
+const setAuthToken = () => {
+    const AuthToken = `Bearer ${localStorage.getItem('JWT')}`
+    axios.defaults.headers.common['Authorization'] = AuthToken
+}
+const requestLike = (data, callback, errorCallback) => {
+    setAuthToken()
+    axios.post(hosturl+appname+'/like', data) // userId, restaurantId
+    .then(callback)
+    .catch(errorCallback)
+}
+const requestList = (callback, errorCallback) => {
+    axios.get(hosturl+appname+'/list')
+    .then(callback)
+    .catch(errorCallback)
+}
+
+const requestInfo = (restaurantId, callback, errorCallback) => {
+    axios.get(hosturl+appname+'/'+restaurantId)
+    .then(callback)
+    .catch(errorCallback)
+}
+
+const requestCreate = (data, callback, errorCallback) => {
+    setAuthToken()
+    axios.post(hosturl+appname+'/create', data)
+    .then(callback)
+    .catch(errorCallback)
+}
+
+const RestaurantApi = {
+    requestLike:(data,callback,errorCallback)=>requestLike(data,callback,errorCallback),
+    requestList:(callback,errorCallback)=>requestList(callback,errorCallback),
+    requestInfo:(RestaurantId,callback,errorCallback)=>requestInfo(RestaurantId,callback,errorCallback),
+    requestCreate:(data,callback,errorCallback)=>requestCreate(data,callback,errorCallback),
+}
+
+export default RestaurantApi
