d79fd55b869f21c2c46dc7938753b4bf0007f9ec
jhj9109 <jhj91_09@naver.com>
2020-08-03 14:08:15 +0900

    Update vue | 레스토랑카드 컴포넌트 적용 |

--- frontend/src/views/review/Restaurant.vue
+++ frontend/src/views/review/Restaurant.vue
@@ -1,65 +1,61 @@
 <template>
-  <v-card
-    :loading="loading"
-    class="mx-auto my-0"
-    max-width="374"
-  >
-    <v-img
-			@click="onClick"
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
+  <div>
+    <RestaurantCard
+      v-for="restaurant in restaurants"
+      :key="restaurant.id"
+      :restaurantInfo="restaurant"
+    />
+  </div>
 
-    <v-card-title @click="onClick">어썸한 가게</v-card-title>
-
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ml-4">4.5 (413)</div>
-      </v-row>
-
-      <div class="my-4 subtitle-1">
-        이탈리안, 카페
-      </div>
-
-      <div>맛있는 이탈리안 음식점, 어썸한 우리 가게로 오세요^^</div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>영업 시간</v-card-title>
-
-    <v-card-text>
-			0:00 ~ 24:00
-    </v-card-text>
-
-  </v-card>
 </template>
 
 <script>
-  export default {
-    name: "Restaurant",
-    data: () => ({
-      loading: false,
-    }),
+import RestaurantCard from '@/components/restaurant/RestaurantCard';
 
-    methods: {
-			onClick(){
-				this.$router.push('/restaurant/review');
-			}
+// import { mapState, mapMutations } from 'vuex';
+
+export default {
+  name: 'Restaurant',
+  components: {
+    RestaurantCard,
+  },
+  data() {
+    return {
+      restaurants: [],
+    }
+  },
+  methods: {
+    // 아래는 임시 데이터 세팅을 위한 메소드
+    tempSetData() {
+      this.restaurants = [
+        {
+          id: 1,
+          title: "어썸한 가게",
+          content: "어썸한 우리가게로 오세요!!!",
+          imgUrl: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
+          types: ["이탈리안","레스토랑"],
+          rank: 4.5,
+          likeCount: 404,
+          deal_date: "10:00 ~ 21:00"
+        },
+        {
+          id: 2,
+          title: "어썸한 두번째 가게",
+          content: "어썸한 우리가게로 오세요!!!",
+          imgUrl: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+          types: ["이탈리안","레스토랑"],
+          rank: 4.7,
+          likeCount: 500,
+          deal_date: "10:00 ~ 21:00"
+        },
+      ]
     },
+  },
+  mounted() {
+    // 아래의 임시 데이터 세팅 메소드 대신, 실제 데이터 세팅 메소드 넣으면 됌
+    this.tempSetData()
   }
+}
 </script>
 <style>
 
---
+++ frontend/src/views/review/RestaurantDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <RestaurantCard 
+      :restaurantInfo="restaurant"
+    />
+  </div>
+
+</template>
+
+<script>
+import RestaurantCard from '@/components/restaurant/RestaurantCard';
+
+// import { mapState, mapMutations } from 'vuex';
+
+export default {
+  name: 'Restaurant',
+  components: {
+    RestaurantCard,
+  },
+  data() {
+    return {
+      restaurant: {
+        id: 1,
+        title: "어썸한 가게",
+        content: "아직 id 받아 데이터 받기 구현 안함!",
+        imgUrl: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
+        types: ["이탈리안","레스토랑"],
+        rank: 4.5,
+        likeCount: 404,
+        deal_date: "10:00 ~ 21:00"
+      }
+    }
+  },
+}
+</script>
+<style>
+
+</style>
