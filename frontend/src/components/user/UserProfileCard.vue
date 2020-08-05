c0ae79789cc4fd8a57b69626169eeca1983bb79b
jhj9109 <jhj91_09@naver.com>
2020-08-05 14:07:09 +0900

    Add vue | 유저 페이지 프로필 카드 컴포넌트화 |
    'profileUser'객체를 props로 받음
    객체 데이터는 'nickname', 'follower', 'following', 'email'
    팔로우 버튼 클릭에 대한 이벤트는
    'onclick' 이름으로 $emit 하여 상위 컴포넌트로 전달

---
+++ frontend/src/components/user/UserProfileCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-card
+        max-width="600"
+        min-height="120"
+        class="mx-auto mb_custom"
+        outlined="outlined"
+        style="margin-top:15px;"
+    >
+        <v-card-actions>
+        <v-list-item>
+            <!-- 아이콘 디자인 변경, 위치 이동 사진 추가 유무에 따른 분기 필요-->
+            <v-icon
+                size="70"
+                color="#F7B675"
+                style="margin:4px 4px 0px -16px;">mdi-account-circle
+            >
+            </v-icon>
+            <v-list-item-content>
+            <v-list-item-title class="body-1">
+                {{profileUser.nickname}}님
+            </v-list-item-title>
+            <!-- 팔로워 팔로잉 변경 -->
+            <v-list-item-subtitle class="mb-1"
+            >
+                팔로워: {{profileUser.follower}}
+                팔로잉: {{ profileUser.following }}
+            </v-list-item-subtitle>
+            <!-- 이메일 -> 자기소개로 변경  -->
+            <v-list-item-subtitle
+            >
+                {{profileUser.email}}
+            </v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+        
+        <!-- 팔로워/팔로잉 디자인 수정 팔로우 했을 경우 팔로우 취소로 변경-->
+        </v-card-actions>
+        <v-layout class="mb-3 mr-3 justify-end">
+            <!--
+            <v-subheader>
+            <v-btn text large color="error">Follower: {{ profileUser.follower }} </v-btn> 
+            <v-btn text large color="error">Following: {{ profileUser.following }} </v-btn> 
+            </v-subheader>
+            -->
+            <v-btn  @click="onClick" dark small color=#F7B675>
+            팔로우
+            </v-btn>
+
+        </v-layout>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: [ 'profileUser'],
+    methods: {
+        onClick() {
+            this.$emit('onFollow')
+            console.log("팔로우 반응")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
