9c8c1b2ee8e707356f0f6250b71727a41b3d33d1
함운경 <wkhham@naver.com>
2020-08-07 12:51:33 +0900

    fix

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // axios 객체 생성
 export default axios.create({
-  baseURL : 'http://localhost:9999',
-  //baseURL: 'http://i3a409.p.ssafy.io:8399',
+  //baseURL : 'http://localhost:9999',
+  baseURL: 'http://i3a409.p.ssafy.io:8399',
   headers: {
     'Content-type': 'application/json',
   },
