cea7ab2ccf01eea6195169e4fb31f2c5d501353a
jhj9109 <jhj91_09@naver.com>
2020-08-19 15:49:40 +0900

    프로필 문구 간격 증가, Card 구조 삭제

--- frontend/src/components/Footerbar.vue
+++ frontend/src/components/Footerbar.vue
@@ -1,44 +1,42 @@
 <template>
-    <v-card height="70px">
-      <v-footer
-        fixed
-        flat
-        tile
-        dark
-        width="100%"
-        height="56%"
-        class="backgroud_gradient"
-      >
-        
-        <v-row justify='center'>
-          <v-btn :to="{ name: 'Restaurant' }" icon>
-            <v-icon size="24px">mdi-search-web</v-icon>
-          </v-btn>
+  <v-footer
+    fixed
+    flat
+    tile
+    dark
+    width="100%"
+    height="56%"
+    class="backgroud_gradient"
+  >
+    
+    <v-row justify='center'>
+      <v-btn :to="{ name: 'Restaurant' }" icon>
+        <v-icon size="24px">mdi-search-web</v-icon>
+      </v-btn>
 
-          <v-btn :to="{ name: 'Map' }" class="ml-3" icon >
-            <v-icon size="24px">mdi-map</v-icon>
-          </v-btn>
-          <!-- 피드로 가는 버튼 추가 -->
-          <v-btn class="ml-3" icon>
-            <v-icon>mdi-format-align-justify</v-icon>
-          </v-btn>
-          
-          <v-btn @click="onClick" class="ml-3" icon>
-            <v-icon>mdi-account</v-icon>
-          </v-btn>
-            <!-- 맨위로 가기(재구현) -->
-            <v-btn @click="$emit('scrollToTop')" class="ml-3" icon>
-              <v-icon>mdi-arrow-up</v-icon>
-            </v-btn>
-          <!-- 글쓰기 버튼 restaurant card로 옮김.
-          <v-btn :to="{ name: 'ReviewCreate' }" class="ml-2" icon >
-            <v-icon size="24px">mdi-lead-pencil</v-icon>
-          </v-btn>
-          -->
-       </v-row>
-       
-      </v-footer>
-    </v-card>
+      <v-btn :to="{ name: 'Map' }" class="ml-3" icon >
+        <v-icon size="24px">mdi-map</v-icon>
+      </v-btn>
+      <!-- 피드로 가는 버튼 추가 -->
+      <v-btn class="ml-3" icon>
+        <v-icon>mdi-format-align-justify</v-icon>
+      </v-btn>
+      
+      <v-btn @click="onClick" class="ml-3" icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+        <!-- 맨위로 가기(재구현) -->
+        <v-btn @click="$emit('scrollToTop')" class="ml-3" icon>
+          <v-icon>mdi-arrow-up</v-icon>
+        </v-btn>
+      <!-- 글쓰기 버튼 restaurant card로 옮김.
+      <v-btn :to="{ name: 'ReviewCreate' }" class="ml-2" icon >
+        <v-icon size="24px">mdi-lead-pencil</v-icon>
+      </v-btn>
+      -->
+    </v-row>
+    
+  </v-footer>
 </template>
 
 <script>
@@ -57,7 +55,7 @@ export default {
 }
 </script>
 <style >
-.backgroud_gradient {
-    background: linear-gradient(90deg, #fa709a, #f6d365 100%);
-}
+  .backgroud_gradient {
+      background: linear-gradient(90deg, #fa709a, #f6d365 100%);
+  }
 </style>
