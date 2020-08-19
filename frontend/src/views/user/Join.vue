0911732b9e6b624ed66110dc722dd764fce8e5e8
함운경 <wkhham@naver.com>
2020-08-19 22:05:11 +0900

    profile 사진

--- frontend/src/views/user/Join.vue
+++ frontend/src/views/user/Join.vue
@@ -52,6 +52,34 @@
             type="password"
             required
           ></v-text-field>
+
+                        <v-text-field
+                            class="pt-0"
+                            label="이미지 첨부"
+                            @click='pickFile'
+                            v-model='profile'
+                            prepend-inner-icon='mdi-camera'>
+                        </v-text-field>
+
+                        <input
+                            type="file"
+                            style="display: none"
+                            ref="image"
+                            accept="image/*"
+                            @change="previewImage"
+                        >
+                       <div v-if="imageData!=null">
+                        <!-- <v-btn
+                            @click="onUpload"
+                        >
+                            업로드 하기
+                        </v-btn>                        -->
+                        <img width=344 height=344 :src="picture">
+                        <p v-if="uploadValue !== 0 && uploadValue !== 100 ">업로드 중입니다... {{uploadValue.toFixed()+"%"}}
+                            <progress id="progress" :value="uploadValue" max="100" />
+                        </p>
+                            
+                     </div>
         </v-col>
             <v-btn
       :disabled="!valid"
@@ -69,6 +97,7 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 
 export default {
   data: () => {
@@ -78,6 +107,7 @@ export default {
       nickName: "",
       password: "",
       passwordConfirm: "",
+      image:'',
       isLoading: false,
       error: {
         email: false,
@@ -103,11 +133,50 @@ export default {
         v => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(v) || '비밀번호는 글자, 숫자 포함 8자 이상입니다.',
         // /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/ : 소문자1+대문자1+특문1+8자 이상
       ],
+      imageData: null,
+      picture: null,
+      uploadValue : 0,
+      profile : '',
 
     };
   },
 
   methods: {
+    pickFile() {
+			console.log("픽파일")
+			console.log(this)
+			console.log(this.$refs)
+			console.log(this.$refs.image) // ref에 등록된 이름 기준으로 찾아냄 => input
+            this.$refs.image.click()
+        },
+        previewImage(event) {
+           this.uploadValue=0;
+           this.picture=null;
+           this.imageData = event.target.files[0];
+           this.onUpload()
+       },
+        onUpload(){
+            this.picture=null;
+            this.profile = `${this.imageData.name}`;
+            const storageRef=firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+            storageRef.on(
+                `state_changed`,
+                snapshot => {
+                    this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                },
+                error => { 
+                    console.log(error.message)
+                },
+                () => {
+                    this.uploadValue=100;
+                    storageRef.snapshot.ref.getDownloadURL().then((url) => {
+                        this.picture =url;
+                        this.image =url;
+                        console.log('url 저장')
+                        //this.postingForm
+                    });
+            });
+        },      
     onJoin(){
       // 클릭단계에서 유효성 검증 이미 진행하였음 => :disable="valid"
       const singUpData = {
